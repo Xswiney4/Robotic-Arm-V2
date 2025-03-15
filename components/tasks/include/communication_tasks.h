7fee322b97e4cca32b503be27fe8ac5f082b5953
Xander <Xswiney4@gmail.com>
2025-03-15 14:57:55 -0500

    Setup a communication initializer, responsible for setting up a queue for user commands.

---
+++ components/tasks/include/communication_tasks.h
@@ -0,0 +1,16 @@
+#ifndef COMMUNICATION_TASKS_H
+#define COMMUNICATION_TASKS_H
+
+
+// Initializations
+void initCommunications(); // Sets up all communication tasks
+void initBluetooth();
+void initUART();
+
+// Handlers
+
+// Task Definitions
+
+// Other Utils
+
+#endif

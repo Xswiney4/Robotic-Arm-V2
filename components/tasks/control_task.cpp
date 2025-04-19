d81dac9d5d19833c8189f35bf55f4bf3845062bd
Xander <Xswiney4@gmail.com>
2025-04-19 17:08:53 -0500

    Updated control task into a class format

--- components/tasks/control_task.cpp
+++ components/tasks/control_task.cpp
@@ -13,105 +13,29 @@
 // Utils
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// ~~ Initializations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~ Constructor/Destructor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// ~~ User Commands ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-/*
-Sets the endpoint given a position (x, y, z) and orientation (pitch, yaw, roll) of the end effector
-The control structure is as follows:
-- UserCommand is sent over to Kinematics Solver to be solved
-- Wait until all motors are ready AND Kinematic Solver flags 'solved'
-- Enable all motors
-- Break
-*/
-void setEnd(UserCommand* cmd){
-    // double x =      cmd->params[0];
-    // double y =      cmd->params[1];
-    // double z =      cmd->params[2];
-    // double pitch =  cmd->params[3];
-    // double yaw =    cmd->params[4];
-    // double roll =   cmd->params[5];
-}
-
-/*
-Sets the endpoint speed in m/s
-The structure is as follows:
-- 
-*/
-void setEndSpeed(UserCommand* cmd){
-    // double endSpeed = cmd->params[0];
-}
-
-/*
-Sets the angle of a given motor in degrees, it uses STEPPER_SPEED as the speed for the motors
-The structure is as follows:
-- Angle is sent over to motor
-- Wait until given motor is ready
-- Enable given motor
-- Break
-*/
-void setMotorAngles(UserCommand* cmd){
-    // float m1Angle =    cmd->params[0];
-    // float m2Angle =    cmd->params[1];
-    // float m3Angle =    cmd->params[2];
-    // float m4Angle =    cmd->params[3];
-    // float m5Angle =    cmd->params[4];
-    // float m6Angle =    cmd->params[5];
-    
-    uint8_t motorBitMask = 0x00;
-
-    // Loop going through each parameter input
-    for(int i = 0; i < 6; i++){
-        // If parameter is valid
-        if(cmd->params[i] != -1.0f){
-
-            // Adds the motor to the global bit mask
-            motorBitMask |= (1 << i);
-        }
-    }
-
-    // Log
-    ESP_LOGD(TASK_NAME_CONTROL, "Motor Bit Mask: 0x%2X", motorBitMask);
-
-    // Wait for all motors to be ready (Once they recieve targets from the kinematics task)
-    xEventGroupWaitBits(motorReady, motorBitMask, pdTRUE, pdTRUE, portMAX_DELAY);
-    
-    // Enable Motors
-    xEventGroupClearBits(motorIdle, motorBitMask);
-    xEventGroupSetBits(motorEnabled, motorBitMask);
-
-    // Wait until all motor's are idle before continuing
-    xEventGroupWaitBits(motorIdle, motorBitMask, pdFALSE, pdTRUE, portMAX_DELAY);
+// Constructor
+ControlTask::ControlTask(){
 
 }
 
-/*
-Sets the given motor speed in m/s
-The structure is as follows:
-- UserCommand is sent over to Kinematics Solver to be solved
-- Wait until given motor is ready AND Kinematic Solver flags 'solved'
-- Break
-*/
-void setMotorSpeed(UserCommand* cmd){
-    // int motor =     (int)cmd->params[0];
-    // double speed =       cmd->params[1];
+// Destructor
+ControlTask::~ControlTask(){
+
 }
 
-// Puts the controller to sleep for a given time in ms
-void sleep(UserCommand* cmd){
-    ESP_LOGD(TASK_NAME_CONTROL, "Delaying control task for %dms", (int)cmd->params[0]);
-    vTaskDelay(pdMS_TO_TICKS((int)cmd->params[0]));
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// ~~ Handlers ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ********************************* PRIVATE **************************************
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~ Task Definitions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-void controlTask(void *pvParameter){
+void ControlTask::taskEntry(void* pvParameters){
+    ControlTask* self = static_cast<ControlTask*>(pvParameters);
+    self->controlTask();
+}
+
+void ControlTask::controlTask(){
 
     // Task Variables
     UserCommand cmd;
@@ -160,10 +84,128 @@ void controlTask(void *pvParameter){
                 break;
         }
 
-
-
     }
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~ User Commands ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/*
+Sets the endpoint given a position (x, y, z) and orientation (pitch, yaw, roll) of the end effector
+The control structure is as follows:
+- UserCommand is sent over to Kinematics Solver to be solved
+- Wait until all motors are ready AND Kinematic Solver flags 'solved'
+- Enable all motors
+- Break
+*/
+void ControlTask::setEnd(UserCommand* cmd){
+    // double x =      cmd->params[0];
+    // double y =      cmd->params[1];
+    // double z =      cmd->params[2];
+    // double pitch =  cmd->params[3];
+    // double yaw =    cmd->params[4];
+    // double roll =   cmd->params[5];
+}
+
+/*
+Sets the endpoint speed in m/s
+The structure is as follows:
+- 
+*/
+void ControlTask::setEndSpeed(UserCommand* cmd){
+    // double endSpeed = cmd->params[0];
+}
+
+/*
+Sets the angle of a given motor in degrees, it uses STEPPER_SPEED as the speed for the motors
+The structure is as follows:
+- Angle is sent over to motor
+- Wait until given motor is ready
+- Enable given motor
+- Break
+*/
+void ControlTask::setMotorAngles(UserCommand* cmd){
+    // float m1Angle =    cmd->params[0];
+    // float m2Angle =    cmd->params[1];
+    // float m3Angle =    cmd->params[2];
+    // float m4Angle =    cmd->params[3];
+    // float m5Angle =    cmd->params[4];
+    // float m6Angle =    cmd->params[5];
+    
+    uint8_t motorBitMask = 0x00;
+
+    // Loop going through each parameter input
+    for(int i = 0; i < 6; i++){
+        // If parameter is valid
+        if(cmd->params[i] != -1.0f){
+
+            // Adds the motor to the global bit mask
+            motorBitMask |= (1 << i);
+        }
+    }
+
+    // Log
+    ESP_LOGD(TASK_NAME_CONTROL, "Motor Bit Mask: 0x%2X", motorBitMask);
+
+    // Wait for all motors to be ready (Once they recieve targets from the kinematics task)
+    xEventGroupWaitBits(motorReady, motorBitMask, pdTRUE, pdTRUE, portMAX_DELAY);
+    
+    // Enable Motors
+    xEventGroupClearBits(motorIdle, motorBitMask);
+    xEventGroupSetBits(motorEnabled, motorBitMask);
+
+    // Wait until all motor's are idle before continuing
+    xEventGroupWaitBits(motorIdle, motorBitMask, pdFALSE, pdTRUE, portMAX_DELAY);
+
+}
+
+/*
+Sets the given motor speed in m/s
+The structure is as follows:
+- UserCommand is sent over to Kinematics Solver to be solved
+- Wait until given motor is ready AND Kinematic Solver flags 'solved'
+- Break
+*/
+void ControlTask::setMotorSpeed(UserCommand* cmd){
+    // int motor =     (int)cmd->params[0];
+    // double speed =       cmd->params[1];
+}
+
+// Puts the controller to sleep for a given time in ms
+void ControlTask::sleep(UserCommand* cmd){
+    ESP_LOGD(TASK_NAME_CONTROL, "Delaying control task for %dms", (int)cmd->params[0]);
+    vTaskDelay(pdMS_TO_TICKS((int)cmd->params[0]));
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~ Other Utils ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// *********************************** PUBLIC **************************************
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~ Task Controls ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Starts the task
+void ControlTask::start(){
+    if(taskHandle != nullptr){
+        ESP_LOGE(TASK_NAME_CONTROL, "Task is already running, returning...");
+        return;
+    }
+    xTaskCreate(&ControlTask::taskEntry, TASK_NAME_CONTROL, TASK_STACK_DEPTH_CONTROL, NULL, TASK_PRIORITY_CONTROL, NULL);
+    ESP_LOGI(TASK_NAME_CONTROL, "Task started");
+}
+
+// Stops the task
+void ControlTask::stop(){
+    vTaskDelete(taskHandle);
+    ESP_LOGI(TASK_NAME_CONTROL, "Task Stopped");
+}
+
+// Restarts the task, if it's running
+void ControlTask::restart(){
+    if(taskHandle != nullptr){
+        stop();
+    }
+    start();
+}
--- components/tasks/include/control_task.h
+++ components/tasks/include/control_task.h
@@ -3,21 +3,39 @@
 
 #include "robotic_definitions.h"
 
-// User Commands
-void setEnd(UserCommand* cmd);
-void setEndSpeed(UserCommand* cmd);
-void setMotorAngles(UserCommand* cmd);
-void setMotorSpeed(UserCommand* cmd);
-void sleep(UserCommand* cmd);
+class ControlTask{
+    private:
 
+        // Task Handle
+        TaskHandle_t taskHandle = nullptr;
 
-// Task Definition
-void controlTask(void *pvParameter);
+        // Task Definition
+        static void taskEntry(void* pvParameters);
+        void controlTask();
 
-// Helper Functions
-void primeMotor(int motor, float desiredAngle, float speed);
-void enableMotors();
-void waitTilMotorsIdle();
+        // User Commands
+        void setEnd(UserCommand* cmd);
+        void setEndSpeed(UserCommand* cmd);
+        void setMotorAngles(UserCommand* cmd);
+        void setMotorSpeed(UserCommand* cmd);
+        void sleep(UserCommand* cmd);
 
+        // Other Utils
+        void primeMotor(int motor, float desiredAngle, float speed);
+        void enableMotors();
+        void waitTilMotorsIdle();
+
+    public:
+
+        // Task Controls
+        void start();
+        void stop();
+        void restart();
+
+        // Constructor/Destructor
+        ControlTask();
+        ~ControlTask();
+
+};
 
 #endif
